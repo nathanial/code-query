d4549d8348fae92e7eee9e1920f2c839cb74203d
Nathan Hartman <nathanial.hartman@gmail.com>
2016-12-02 03:54:29 -0800

    added repository details page

--- client/RepositoryOverview.jsx
+++ client/RepositoryOverview.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Button} from '@blueprintjs/core';
 
 export default class RepositoryOverview extends React.Component {
 
+	static propTypes = {
+		id: React.PropTypes.number
+	}
+
 	render(){
+		console.log("ID", this.props.params.id);
 		return (
 			<div className="repository-overview">
-				<h1>Good Times</h1>
+				<h1>{this.props.id}</h1>
+				<Button iconName="trash" className="delete-btn pt-intent-danger" text="Delete" />
 			</div>
 		);
 	}
